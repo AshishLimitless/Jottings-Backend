d1c0df8443c9028dfdcd61ec9f66fa4e39dbea0f
AshishLimitless <karraashishreddy4480@gmail.com>
2023-10-02 20:00:51 +0530

    usermodel and other changed

---
+++ schemas/Usermodels.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { Trim } from "./../node_modules/mongoose/types/expressions.d";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.default("users", userSchema);
---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bparser = require("body-parser");
+
+const app = express();
+
+//port configuration
+const PORT = 8080;
+
+//routes
+
+app.use();
